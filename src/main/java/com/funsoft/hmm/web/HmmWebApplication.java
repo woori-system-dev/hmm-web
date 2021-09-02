c662becd20e37e7cc8f107b8a4c1d830450e3569
Woorisystem <jhlee20110907@gmail.com>
2021-09-02 12:04:50 +0900

    websocket : 알람메시지를 받기위한 웹소켓 추가

--- src/main/java/com/funsoft/hmm/web/HmmWebApplication.java
+++ src/main/java/com/funsoft/hmm/web/HmmWebApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class HmmWebApplication extends SpringBootServletInitializer {
@@ -12,6 +14,11 @@ public class HmmWebApplication extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(HmmWebApplication.class);
 	}
+	
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HmmWebApplication.class, args);
--- src/main/java/com/funsoft/hmm/web/service/info/CdmaMessageProcessor.java
+++ src/main/java/com/funsoft/hmm/web/service/info/CdmaMessageProcessor.java
@@ -1,6 +1,10 @@
 package com.funsoft.hmm.web.service.info;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,6 +13,8 @@ import com.funsoft.hmm.netty.cdma.msg.FlowReport;
 import com.funsoft.hmm.netty.cdma.msg.common.CdmaIncomingMessage;
 import com.funsoft.hmm.netty.listener.MessageListener;
 import com.funsoft.hmm.web.common.DateUtil;
+import com.funsoft.hmm.web.domain.db.AlarmDevice;
+import com.funsoft.hmm.web.domain.db.BlockSmall;
 import com.funsoft.hmm.web.domain.db.Holiday;
 import com.funsoft.hmm.web.domain.db.NightMinFlow;
 import com.funsoft.hmm.web.domain.db.RealTimeMeasurement;
@@ -59,9 +65,21 @@ public class CdmaMessageProcessor implements MessageListener<CdmaIncomingMessage
 	}
 
 	private void processDeviceReport(CdmaIncomingMessage in) {
+		
 		FlowReport report = (FlowReport) in;
 
 		RealTimeMeasurement measurement = report.getMeasurement();
+		
+		/*
+		 * TODO
+		 * 1. 수압 알람을 위해 직전 값을 가져온다.
+		 * 2. 쓰레드로 등록 해야될것 같음
+		 * 3. 사용자가 수압의 격차 범위를 설정할 수 있도록 해야 함
+		 * 4. 웹소켓에 연결된 클라이언트에게 알람 내용을 전송해야 함
+		 */
+		RealTimeMeasurement oldRtm= realTimeMeasurementService.getRecentData(measurement.getBkFlctcFm());
+				
+		
 		Holiday holiday = holidayService.get(DateUtil.toSmallDateString(measurement.getDatetime()));
 		measurement.setHoliday(holiday.isHoliday());
 		realTimeMeasurementService.regist(measurement);
@@ -73,8 +91,16 @@ public class CdmaMessageProcessor implements MessageListener<CdmaIncomingMessage
 			nightMinFlow.setAllowFlow(alarmThresholdService.get(measurement.getBkFlctcFm()).getAllowFlow());
 			nightMinFlowService.regist(nightMinFlow);
 		}
-
-		System.err.println("TEST REPORT : " + report.toString());
+		
+		//쓰레드로 등록하여 웹접속자가 알람을 받을 수 있어야 한다.
+		//50을 사용자가 설정한 값으로 변경해야함, 적용될려면 DB에 저장된 내용을 항상 가져와야 함
+		if(Math.abs(oldRtm.getPressure() - measurement.getPressure()) > 50) {
+			
+			
+		}
 	}
+	
+	
+
 
 }
